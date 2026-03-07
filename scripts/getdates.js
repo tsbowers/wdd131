d93e3ef9294bc41e02a833c60c480932f62dd2ee
tsbowers <trevorbowers3@gmail.com>
2026-03-07 16:49:47 -0600

    grt date fix

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
